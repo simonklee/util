68b88b9371c999dd5b9168147664e63ecca338aa
Simon Zimmermann <simon@insmo.com>
2013-10-23 20:44:59 +0200

    make Severity public

--- log/log.go
+++ log/log.go
@@ -13,8 +13,6 @@ import (
 	"github.com/simonz05/util/raven"
 )
 
-type severity int32
-
 const (
 	LevelFatal Level = iota
 	LevelError
@@ -22,14 +20,15 @@ const (
 )
 
 var (
+	// Severity stores the log level
+	Severity Level
 	std      logger
-	sev      Level
 	filename *string
 	ravenDSN *string
 )
 
 func init() {
-	flag.Var(&sev, "log", "log level")
+	flag.Var(&Severity, "log", "log level")
 	filename = flag.String("log-file", "", "If non-empty, write log to this file")
 	ravenDSN = flag.String("log-raven-dsn", "", "If non-empty, write to raven dsn")
 	std = new(multiLogger)
@@ -60,10 +59,10 @@ func (l multiLogger) Output(calldepth int, s string, sev Level) (err error) {
 }
 
 func (l *multiLogger) init() {
-	l.loggers = append(l.loggers, &consoleLogger{sev: sev})
+	l.loggers = append(l.loggers, &consoleLogger{sev: Severity})
 
 	if *filename != "" {
-		l.loggers = append(l.loggers, &fileLogger{fname: *filename, sev: sev})
+		l.loggers = append(l.loggers, &fileLogger{fname: *filename, sev: Severity})
 	}
 
 	if *ravenDSN != "" {
@@ -200,7 +199,7 @@ func (l *Level) Set(value string) error {
 // Print calls Output to print to the standard logger.
 // Arguments are handled in the manner of fmt.Print.
 func Print(v ...interface{}) {
-	if sev >= LevelInfo {
+	if Severity >= LevelInfo {
 		std.Output(5, fmt.Sprint(v...), LevelInfo)
 	}
 }
@@ -208,7 +207,7 @@ func Print(v ...interface{}) {
 // Printf calls Output to print to the standard logger.
 // Arguments are handled in the manner of fmt.Printf.
 func Printf(format string, v ...interface{}) {
-	if sev >= LevelInfo {
+	if Severity >= LevelInfo {
 		std.Output(5, fmt.Sprintf(format, v...), LevelInfo)
 	}
 }
@@ -216,35 +215,35 @@ func Printf(format string, v ...interface{}) {
 // Println calls Output to print to the standard logger.
 // Arguments are handled in the manner of fmt.Println.
 func Println(v ...interface{}) {
-	if sev >= LevelInfo {
+	if Severity >= LevelInfo {
 		std.Output(5, fmt.Sprintln(v...), LevelInfo)
 	}
 }
 
 // Fatal is equivalent to Print() followed by a call to os.Exit(1).
 func Error(v ...interface{}) {
-	if sev >= LevelError {
+	if Severity >= LevelError {
 		std.Output(5, fmt.Sprint(v...), LevelError)
 	}
 }
 
 // Fatalf is equivalent to Printf() followed by a call to os.Exit(1).
 func Errorf(format string, v ...interface{}) {
-	if sev >= LevelError {
+	if Severity >= LevelError {
 		std.Output(5, fmt.Sprintf(format, v...), LevelError)
 	}
 }
 
 // Fatalln is equivalent to Println() followed by a call to os.Exit(1).
 func Errorln(v ...interface{}) {
-	if sev >= LevelError {
+	if Severity >= LevelError {
 		std.Output(5, fmt.Sprintln(v...), LevelError)
 	}
 }
 
 // Fatal is equivalent to Print() followed by a call to os.Exit(1).
 func Fatal(v ...interface{}) {
-	if sev >= LevelFatal {
+	if Severity >= LevelFatal {
 		std.Output(5, fmt.Sprint(v...), LevelFatal)
 		os.Exit(1)
 	}
@@ -252,7 +251,7 @@ func Fatal(v ...interface{}) {
 
 // Fatalf is equivalent to Printf() followed by a call to os.Exit(1).
 func Fatalf(format string, v ...interface{}) {
-	if sev >= LevelFatal {
+	if Severity >= LevelFatal {
 		std.Output(5, fmt.Sprintf(format, v...), LevelFatal)
 		os.Exit(1)
 	}
@@ -260,7 +259,7 @@ func Fatalf(format string, v ...interface{}) {
 
 // Fatalln is equivalent to Println() followed by a call to os.Exit(1).
 func Fatalln(v ...interface{}) {
-	if sev >= LevelFatal {
+	if Severity >= LevelFatal {
 		std.Output(5, fmt.Sprintln(v...), LevelFatal)
 		os.Exit(1)
 	}
