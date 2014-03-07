7e6f21d8f97ea17f0c574f21c44dfbc5d9b17260
Simon Zimmermann <simon@insmo.com>
2014-03-07 12:11:15 +0100

    add modified pat from gorilla project

---
+++ pat/pat.go
@@ -0,0 +1,45 @@
+// Copyright 2012 The Gorilla Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package pat
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+// Creates a stack of HTTP handlers. Each HTTP handler is responsible for
+// calling the next. The handlers are executed in reverse order, the last is
+// called first.
+func use(handler http.Handler, middleware ...func(http.Handler) http.Handler) http.Handler {
+	for _, m := range middleware {
+		handler = m(handler)
+	}
+	return handler
+}
+
+// registerPat a pattern with a handler for the given request method.
+func registerPat(r *mux.Router, meth, pat string, h http.Handler, middleware []func(http.Handler) http.Handler) *mux.Route {
+	return r.NewRoute().PathPrefix(pat).Handler(use(h, middleware...)).Methods(meth)
+}
+
+// Delete registers a pattern with a handler for DELETE requests.
+func Delete(r *mux.Router, pat string, h http.Handler, middleware ...func(http.Handler) http.Handler) *mux.Route {
+	return registerPat(r, "DELETE", pat, h, middleware)
+}
+
+// Get registers a pattern with a handler for GET requests.
+func Get(r *mux.Router, pat string, h http.Handler, middleware ...func(http.Handler) http.Handler) *mux.Route {
+	return registerPat(r, "GET", pat, h, middleware)
+}
+
+// Post registers a pattern with a handler for POST requests.
+func Post(r *mux.Router, pat string, h http.Handler, middleware ...func(http.Handler) http.Handler) *mux.Route {
+	return registerPat(r, "POST", pat, h, middleware)
+}
+
+// Put registers a pattern with a handler for PUT requests.
+func Put(r *mux.Router, pat string, h http.Handler, middleware ...func(http.Handler) http.Handler) *mux.Route {
+	return registerPat(r, "PUT", pat, h, middleware)
+}
